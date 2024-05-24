10297ff0a489deb6091057c38f63967cda297256
Shaul Fridman <shaulf@nvidia.com>
2024-05-24 16:18:13 +0300

    (+) parallel exctraction

--- k600_extractor.sh
+++ k600_extractor.sh
@@ -8,35 +8,31 @@ root_dl_targz="k600_targz"
 [ ! -d $root_dl_targz ] && echo -e "\nRun k600_downloaders.sh"
 [ ! -d $root_dl ] && mkdir $root_dl
 
-# Extract train
-curr_dl=$root_dl_targz/train
-curr_extract=$root_dl/train
-[ ! -d $curr_extract ] && mkdir -p $curr_extract
-find $curr_dl -type f | while read file; do mv "$file" $(echo $file | tr ' ' '_'); done
-tar_list=$(ls $curr_dl)
-for f in $tar_list; do
-	[[ $f == *.tar.gz ]] && echo Extracting $curr_dl/$f to $curr_extract && tar zxf $curr_dl/$f -C $curr_extract
-done
+function extract_tar_gz() {
+	local folder_name=$1
+	local curr_dl="${root_dl_targz}/${folder_name}"
+	local curr_extract="${root_dl}/${folder_name}"
 
-# Extract validation
-curr_dl=$root_dl_targz/val
-curr_extract=$root_dl/val
-[ ! -d $curr_extract ] && mkdir -p $curr_extract
-find $curr_dl -type f | while read file; do mv "$file" $(echo $file | tr ' ' '_'); done
-tar_list=$(ls $curr_dl)
-for f in $tar_list; do
-	[[ $f == *.tar.gz ]] && echo Extracting $curr_dl/$f to $curr_extract && tar zxf $curr_dl/$f -C $curr_extract
-done
+	[ ! -d "$curr_extract" ] && mkdir -p "$curr_extract"
 
-# Extract test
-curr_dl=$root_dl_targz/test
-curr_extract=$root_dl/test
-[ ! -d $curr_extract ] && mkdir -p $curr_extract
-find $curr_dl -type f | while read file; do mv "$file" $(echo $file | tr ' ' '_'); done
-tar_list=$(ls $curr_dl)
-for f in $tar_list; do
-	[[ $f == *.tar.gz ]] && echo Extracting $curr_dl/$f to $curr_extract && tar zxf $curr_dl/$f -C $curr_extract
-done
+	find "$curr_dl" -type f -name '*.tar.gz' | while read file; do
+		local new_file=$(echo "$file" | tr ' ' '_')
+		if [[ "$file" != "$new_file" ]]; then
+			mv "$file" "$new_file"
+		fi
+	done
+
+	find "$curr_dl" -type f -name '*.tar.gz' | parallel \
+		'echo Extracting {} to '"$curr_extract"' && tar zxf {} -C '"$curr_extract"''
+}
+
+# Run function for each directory in the background
+extract_tar_gz "test" &
+extract_tar_gz "val" &
+extract_tar_gz "train" &
+
+# Wait for all background jobs to complete
+wait
 
 # Extraction complete
 echo -e "\nExtractions complete!"
